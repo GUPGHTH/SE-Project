66ea88ed0f43da6e616fe91b43ef98ab4bede423
GUPGHTH <pptoncom@gmail.com>
2023-04-09 01:08:21 +0700

    new change

--- client/src/Component/Home.js
+++ client/src/Component/Home.js
@@ -1,20 +1,19 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
+
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import '../Styles/Home.css';
 
 export default function Home() {
   return (
-    <React.Fragment>
-      <CssBaseline />
+
       <div class="headerbg">
         <div class="container">
             <div class="information">
                 <h1>See you again</h1>
                 <p>It's been along day... without you my friend.</p>
                 <br></br>
-                <a href="#">Listen to this music</a>
+                <a href="https://youtu.be/RgKAFK5djSk">Listen to this music</a>
             </div>
             <div class="video">
               <iframe
@@ -25,23 +24,8 @@ export default function Home() {
               </iframe>   
             </div>
                               
-
         </div> 
 
-
-
-
-
-      </div>
-     
-
-        
-      
-      
-                  
-            
-     
-            
-    </React.Fragment>
+      </div>    
   );
 }
--- client/src/Component/See_detail.js
+++ client/src/Component/See_detail.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Axios from 'axios';
 import { useState } from 'react';
 import {  Link, useParams } from 'react-router-dom';
-
+import '../Styles/See_detail.css'
 
 export default function See_detail(){
 
@@ -46,10 +46,21 @@ export default function See_detail(){
 
     return(
 
-        <div>
-            <p>{booklist[0]?.Book_ID||""}</p>
-            {check()}
-        </div>
+        <body className='body_See_detail'>
+
+            <div className='See_detail'>
+
+                <div className='img_detail'>
+                <img src="https://images.unsplash.com/photo-1592496431122-2349e0fbc666?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1212&q=80" alt=""></img>
+                </div>
+                
+                <div className='text_bt'>
+                    <p>Lorem</p>
+                    <button>ซื้อ</button>
+                </div>
+            </div>
+        </body>
+        
     );
 
 }
