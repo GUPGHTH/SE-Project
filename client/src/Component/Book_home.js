65f89182cbdb0f903fa20fda8c2e249cface51cd
Niwat <pptoncom@gmail.com>
2023-04-10 17:44:29 +0700

    Navbar-search-button

--- client/src/Component/Book_home.js
+++ client/src/Component/Book_home.js
@@ -3,7 +3,6 @@ import Axios from 'axios';
 import { useState } from 'react';
 import { Link ,useParams} from 'react-router-dom';
 import '../Styles/gridbox.css'
-import { Fa500Px } from "react-icons/fa";
 
 export default function Book_home() {
     const [booklist,setbooklist] = useState([]);
@@ -19,8 +18,10 @@ export default function Book_home() {
   
   return (
     <body className='Book_home'>
-      <div className='promosion'>
-                           <img src="https://images.unsplash.com/photo-1592496431122-2349e0fbc666?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1212&q=80" alt=""></img></div>
+      <div class="group">
+        
+        <input  placeholder="Search" type="search" class="input"/>
+      </div>
     <div className="books" >
     {booklist.map((val, key) =>{
         return (
@@ -32,14 +33,13 @@ export default function Book_home() {
                                        <Link to={"/See_detail/"+val.Book_ID}>
                                        <button className="button-28" >See detail</button>
                                        </Link> 
-                                       </div>
+            </div>
               
            
              
             
-        )
-    })}
-</div>
-</body>
+      )})}
+      </div>
+    </body>
   );
 }
--- client/src/Component/Navbar.js
+++ client/src/Component/Navbar.js
@@ -1,18 +1,37 @@
 import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "../Styles/navbar.css";
-import { Fa500Px } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 function Navbar() {
+	const navRef = useRef();
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
 	
 	return (
 		<header>
-			<nav >
-				<a href="/Book_shelf"><Fa500Px/></a>
+			<h1> SanookkidSanookarn</h1>
+			
+			<nav ref={navRef}>
+				<a href="/Book_shelf">หนังสือ</a>
 				<a href="/#">เกี่ยวกับ</a>
 				<a href="/#">ติดต่อ</a>
 				<a href="/#">การสั่งซื้อ</a> 
 				<a href="/login">เข้าสู่ระบบ</a> 
+				
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
 			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
 		</header>
 	);
 }
