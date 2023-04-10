3534a876762835cb8acbb30b201212ea0ac9b34f
GUPGHTH <pptoncom@gmail.com>
2023-04-10 22:46:13 +0700

    Link-Register

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Axios from 'axios'
-import Book_home from './Component/Book_home';
+import Book_shelf from './Component/Book_shelf';
 import Navbar from './Component/Navbar.js';
 import Home from './Component/Home';
 import Layout from './Component/Layout';
@@ -14,7 +14,9 @@ import Edit_Book from './Component/Edit_book';
 import See_detail from './Component/See_detail';
 import Login from './Component/Login';
 import Login_state from './Component/Login_state';
-import Sign_up from'./Component/Sign_up';
+
+import Contact from './Component/Contact';
+import Register from './Component/Register';
 import {
   BrowserRouter,
   Switch,
@@ -22,7 +24,7 @@ import {
   Routes,
   Redirect,
 } from "react-router-dom";
-import Sing_up from './Component/Login';
+
 
 
 
@@ -49,14 +51,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home/>} />
-          <Route path="Book_shelf" element={<Book_home/>} />
+          <Route path="Book_shelf" element={<Book_shelf/>} />
           <Route path="Add_book" element={<Add_book/>} />
           <Route path="Manage_book" element={<Manage_book/>} />
           <Route path="Edit_book/:b_ID" element={<Edit_Book/>}></Route>
           <Route path="See_detail/:b_ID" element={<See_detail/>}></Route>
           <Route path="login" element={<Login/>}></Route>
           <Route path="Login_state" element={<Login_state/>}></Route>
-          <Route path="Sign_up" element={<Sign_up/>}></Route>
+          <Route path="Register" element={<Register/>}></Route>
+          <Route path="Contact" element={<Contact/>}></Route>
           
         </Route>
       </Routes>
